9c18cf020b100b427e817eb1dad3a581101055e7
Sophia <opyrose12@yahoo.com>
2024-11-22 12:35:47 -0500

    added the product seeds and index file

---
+++ prisma/seed.js
@@ -0,0 +1,18 @@
+const prisma = require("../prisma");
+
+const seed = async (numProducts = 25)=>{  
+
+        const products = Array.from({ length: numProducts }, (_,i) =>({
+        title: `Product ${i+1}`,
+        description: `This is a great product!`,
+        price: 1 + Math.floor(Math.random() * 100),
+        }));
+        await prisma.product.createMany({ data: products})
+};
+seed()
+    .then(async ()=> await prisma.$disconnect())
+    .catch(async (e)=>{
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
